b9a7e4d6ee118bd9a574bff46713f1b47af4b063
Alex Oliveira <alexoliveira7x@gmail.com>
2024-05-26 20:08:58 +0100

    chore: implement more specs

--- domain/src/test/java/io/lekito/admin/catalogo/domain/category/CategoryTest.java
+++ domain/src/test/java/io/lekito/admin/catalogo/domain/category/CategoryTest.java
@@ -27,10 +27,38 @@ public class CategoryTest {
 
     @Test
     public void givenAnInvalidName_whenCallNewCategory_thenReceiveError() {
-        final String expectedName = null;
         final var expectedDescription = "Most viewed";
         final var expectedActive = true;
         final var expectErrorMessage = "'name' cannot be null";
+        final var expectedErrorCount = 1;
+
+        Category category = Category.newCategory(null, expectedDescription, expectedActive);
+        final var instanceError = Assertions.assertThrows(DomainException.class, () -> category.validate(new ThrowsValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorCount, instanceError.getErrors().size());
+        Assertions.assertEquals(expectErrorMessage, instanceError.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAnInvalidEmptyName_whenCallNewCategory_thenReceiveError() {
+        final var expectedDescription = "Most viewed";
+        final var expectedActive = true;
+        final var expectErrorMessage = "'name' cannot be empty";
+        final var expectedErrorCount = 1;
+
+        Category category = Category.newCategory(" ", expectedDescription, expectedActive);
+        final var instanceError = Assertions.assertThrows(DomainException.class, () -> category.validate(new ThrowsValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorCount, instanceError.getErrors().size());
+        Assertions.assertEquals(expectErrorMessage, instanceError.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAnInvalidNameLengthLessThan3_whenCallNewCategory_thenReceiveError() {
+        final String expectedName = "Al";
+        final var expectedDescription = "Most viewed";
+        final var expectedActive = true;
+        final var expectErrorMessage = "'name' must be between 3 and 255 characters";
         final var expectedErrorCount = 1;
 
         Category category = Category.newCategory(expectedName, expectedDescription, expectedActive);
@@ -38,5 +66,39 @@ public class CategoryTest {
 
         Assertions.assertEquals(expectedErrorCount, instanceError.getErrors().size());
         Assertions.assertEquals(expectErrorMessage, instanceError.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAnInvalidNameLengthMoreThan255_whenCallNewCategory_thenReceiveError() {
+        final String expectedName = "Percebemos, cada vez mais, que a determinação clara de objetivos garante a contribuição de um grupo importante na determinação das novas proposições. A nível organizacional, a complexidade dos estudos efetuados cumpre um papel essencial na formulação das posturas dos órgãos dirigentes com relação às suas atribuições. O empenho em analisar a constante divulgação das informações agrega valor ao estabelecimento dos conhecimentos estratégicos para atingir a excelência. Neste sentido, a consolidação das estruturas obstaculiza a apreciação da importância dos modos de operação convencionais";
+        final var expectedDescription = "Most viewed";
+        final var expectedActive = true;
+        final var expectErrorMessage = "'name' must be between 3 and 255 characters";
+        final var expectedErrorCount = 1;
+
+        Category category = Category.newCategory(expectedName, expectedDescription, expectedActive);
+        final var instanceError = Assertions.assertThrows(DomainException.class, () -> category.validate(new ThrowsValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorCount, instanceError.getErrors().size());
+        Assertions.assertEquals(expectErrorMessage, instanceError.getErrors().get(0).message());
+    }
+
+    @Test
+    public void givenAnEmptyDescription_whenCallNewCategory_thenNotReceiveError() {
+        final var expectedDescription = " ";
+        final var expectedActive = true;
+
+        Category category = Category.newCategory("Lekito", expectedDescription, expectedActive);
+        Assertions.assertDoesNotThrow(() -> category.validate(new ThrowsValidationHandler()));
+    }
+
+    @Test
+    public void givenAnActiveFalse_whenCallNewCategory_thenNotReceiveError() {
+        final var expectedDescription = " ";
+        final var expectedActive = false;
+
+        Category category = Category.newCategory("Lekito", expectedDescription, expectedActive);
+        Assertions.assertDoesNotThrow(() -> category.validate(new ThrowsValidationHandler()));
+        Assertions.assertEquals(expectedActive, category.isActive());
     }
 }
